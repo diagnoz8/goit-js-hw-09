d649d9fc042343aa8c97306495afae6de550f128
Di <diana8burkut@gmail.com>
2025-03-02 19:42:22 +0100

    task2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,41 @@
 console.log("Form");
+const formData =  {
+email: "", 
+message: ""
+}
+const localStorageKey = "feedback-form-state";
+const feedbackForm = document.querySelector(".feedback-form")
+const email = feedbackForm.email;
+const message = feedbackForm.message;
+const savedData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+email.value = savedData.email ?? "";
+message.value = savedData.message ?? "";
+
+feedbackForm.addEventListener("input", (evt) => { 
+    
+    if
+        (evt.target.type === "email")
+    {
+        formData.email = evt.target.value.trim(); 
+        
+    }
+    
+    else if
+        (evt.target.tagName === "TEXTAREA")
+    {
+        formData.message = evt.target.value.trim(); 
+        
+    }
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+feedbackForm.addEventListener("submit", (evt) =>{
+    evt.preventDefault();
+    if (formData.email === "" || formData.message === "" ) {
+        alert("Fill please all fields")
+    }
+    else{
+      console.log(formData);
+      localStorage.removeItem(localStorageKey);
+      feedbackForm.reset();
+    }
+})
